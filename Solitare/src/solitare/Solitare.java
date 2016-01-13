4b07aa1cd6fe6f9df1582dd2e7533fe83db48a4f
bzim1111 <bill.zimlinghaus@hotmail.com>
2016-01-13 11:20:54 -0500

    Implemented playing field stacks

--- Solitare/src/solitare/Solitare.java
+++ Solitare/src/solitare/Solitare.java
@@ -3,35 +3,47 @@ package solitare;
 public class Solitare {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Deck deck;
-		Card c;
-		Stack s;
+		Stack[] stack = new Stack[7];
+		Stack[] pile = new Stack[4];
 		
 		deck = new Deck();
-		c = null;
-		s = new Stack();
+		for ( int i=0; i<7; i++ ) stack[i] = new Stack();
+		for ( int i=0; i<4; i++ ) pile[i] = new Stack();
 		
 		deck.ShuffleDeck();
 		
-		for ( int i=0; i<10; i++ ) {
-			
-			try {
-				c = deck.Draw();
-			}
-			catch ( OutOfCards ooc ) {
-				System.out.println("out of cards");
-			}
-		
-			c.PrintCard();
-		
-			s.AddCard ( c , false );
-		
-		}
-		
-		s.MakeTopVisible();
-		s.PrintStack();
+		load_stacks ( deck, stack );
+				
+		for ( int i=0; i<7; i++ ) stack[i].PrintStack();
 	}
 
+	
+	/*
+	 * Load the 7 playing field stacks
+	 */
+	
+	static void load_stacks ( Deck d, Stack[] s ) {
+		
+		Card c = null;
+			
+		for ( int i=0; i<7; i++ ) {
+		
+			for ( int j=i; j<7; j++ ) {
+				try {
+					c = d.Draw();
+				}
+				catch ( OutOfCards ooc ) {
+					System.out.println("out of cards");
+				}
+
+				s[6-i].AddCard ( c, false );
+			}
+			
+		}
+		
+		for ( int i=0; i<7; i++ ) s[i].MakeTopVisible();
+	}
+	
 }
